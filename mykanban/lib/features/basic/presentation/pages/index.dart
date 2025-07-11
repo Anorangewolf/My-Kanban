0c57ce845f96115a6ffb5d9783ce7ba376c8741e
Anorangewolf <2148976244@qq.com>
2025-07-11 23:01:27 +0800

    **STYLE**: Prepared some .dart files for UI.

---
+++ mykanban/lib/features/basic/presentation/pages/index.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+/// Ê×Ò³
+class Index extends StatelessWidget {
+  /// Ê×Ò³
+  const Index({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Ê×Ò³',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+    );
+  }
+}
