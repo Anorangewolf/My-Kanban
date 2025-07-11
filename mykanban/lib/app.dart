96db5db80167e9e1e0174c2f8a7d4fef7c167980
Anorangewolf <2148976244@qq.com>
2025-07-11 23:02:54 +0800

    **FEAT**: Added entrance to the app - *renew* app.dart. - *modified* main.dart.

---
+++ mykanban/lib/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+/// 主应用入口
+class Mykanban extends StatelessWidget {
+  /// 主应用
+  const Mykanban({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
