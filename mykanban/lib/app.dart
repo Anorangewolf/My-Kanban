c84c1fc59fa85eb5e4561a6a3317c0862300e6a2
Anorangewolf <2148976244@qq.com>
2025-07-12 22:49:20 +0800

    **BUILD**: Setted up the android build.gradle.kts file.

--- mykanban/lib/app.dart
+++ mykanban/lib/app.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 
 /// 主应用入口
 class Mykanban extends StatelessWidget {
-  /// 主应用
+  /// 主应用入口
   const Mykanban({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const MaterialApp(title: 'Mykanban');
   }
 }
