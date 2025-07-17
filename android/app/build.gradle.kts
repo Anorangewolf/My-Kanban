d01bf098a2b812c18d007afec91bfaf44d302df9
Anorangewolf <2148976244@qq.com>
2025-07-18 03:03:14 +0800

    **CHORE**: Changed the location of the .git folder - *move* /.git to the root of the project.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,15 +8,16 @@ plugins {
 android {
     namespace = "top.anorangewolf.mykanban"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    // ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.2.12479018"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
