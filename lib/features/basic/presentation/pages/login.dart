5d1168e55751195fcc8a924263a40b63bd04ea0f
Anorangewolf <2148976244@qq.com>
2025-07-29 00:05:54 +0800

    **FEAT**: Added login page
    - *add* a simple login page, To be improved later.(This is a super small submission. As a learner, I need to give myself sense of achievement.)

--- lib/features/basic/presentation/pages/login.dart
+++ lib/features/basic/presentation/pages/login.dart
@@ -7,6 +7,46 @@ class Login extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Align(
+        child: FractionallySizedBox(
+          widthFactor: 0.95,
+          heightFactor: 0.95,
+          child: FractionallySizedBox(
+            widthFactor: 0.5,
+            heightFactor: 0.7,
+            child: Container(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Text('登陆'),
+                  const SizedBox(height: 20),
+                  const TextField(
+                    decoration: InputDecoration(
+                      labelText: '用户名',
+                      hintText: '请输入用户名',
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  const TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: '密码',
+                      hintText: '请输入密码',
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('登陆'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
