55bf54c9549ff6a6651dd3cc7c5f497e65ec0a40
Anorangewolf <2148976244@qq.com>
2025-07-28 22:50:57 +0800

    **TEST**: Added some test cases.
    - *add* main_test.dart for simplest startup test.
    - *add* pages_test.dart for quick launch test of pages.

---
+++ test/features/basic/presentation/pages/pages_test.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:mykanban/features/basic/presentation/pages/index.dart';
+
+/// 快速启动测试应用
+class PagesTest extends StatelessWidget {
+  /// 构造函数
+  const PagesTest({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'PagesTest',
+      // 此处home后面放置要测试的页面
+      home: Login(),
+    );
+  }
+}
+
+void main() {
+  runApp(const PagesTest());
+}
